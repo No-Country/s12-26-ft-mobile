3b25488d66dda4d959c3ff4280916c894502b97d
ChristopherPeralta <cperaltapenaloza@gmail.com>
2023-12-07 23:46:39 -0500

    Entity_Room

---
+++ server/NoCountry/src/main/kotlin/com/example/entity/Room.kt
@@ -0,0 +1,19 @@
+package com.example.entity
+
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.Table
+class Room : Table() {
+    val id = integer("id")
+        .autoIncrement()
+    val image = varchar("image", 150)
+    val title = varchar("title", 150)
+    val city = varchar("city", 150)
+    val district = varchar("district", 150)
+    val province = varchar("province", 150)
+    val monthPrice = double("month_price")
+    val sizeM2 = double("size_m2")
+    val isPet = bool("is_pet")
+    val isSmokers = bool("is_smokers")
+    val room = integer("room").references(RoomType().id, onDelete = ReferenceOption.CASCADE)
+    override val primaryKey = PrimaryKey(id, name = "PK_RoomTable_Id")
+}
